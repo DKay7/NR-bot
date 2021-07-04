99e71bca72125c4b3e6e1c3321464fd0cf7b953b
Danny <bublikplushka@yandex.ru>
2021-07-04 21:28:05 +0300

    Fix some bugs

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -9,9 +9,7 @@ from keyboards.default.reply_keyboards import get_cancel_kb, get_admin_kb
 from loader import dp
 from sheets.connect import sp
 from states.states import Admin
-from utils.db_api.db_commands import get_ticket_by_id, add_ticket, get_actual_tickets, archive, get_all_masters
-from utils.db_api.db_commands import set_master_status_deleted, get_master_by_uid
-from keyboards.inline.inline_keyboards import get_ticket_admin_cancel, get_master_admin_cancel
+from utils.db_api.db_commands import get_ticket_by_id, add_ticket
 
 
 @dp.message_handler(Text(equals='Внести заявку'), chat_type=types.ChatType.PRIVATE, state=Admin.default)
@@ -87,55 +85,4 @@ async def bot_start(message: types.Message, state: FSMContext):
     sp.update_table(ticket=get_ticket_by_id(id), table='tickets')
     await mailing(text, message.bot, id)
 
-@dp.message_handler(Text(equals='АРХИВ'), chat_type=types.ChatType.PRIVATE, state=Admin.default)
-async def bot_start(message: types.Message, state: FSMContext):
-    tickets = get_actual_tickets()
 
-
-    if not tickets:
-        await message.answer("На данный момент нет открытых заявок с неназначенным мастером.")
-
-    else:
-        for data in tickets:
-            text = f"Заявка {data['id']}\n\n" \
-                   f"Адрес: {data['address']}\n" \
-                   f"Категория: {data['category']}\n" \
-                   f"Дата выполнения: {data['date']}\n" \
-                   f"Стоимость: {data['price']}\n" \
-                   f"Имя: {data['name']}\n" \
-                   f"Описание:\n" \
-                   f"{data['desc']}"
-
-            await message.answer(text, reply_markup=get_ticket_admin_cancel(data['id']))
-
-
-@dp.callback_query_handler(Text(startswith=f'ticket_archive_'), chat_type=types.ChatType.PRIVATE, state='*')
-async def bot_start(call: types.CallbackQuery, state: FSMContext):
-    await call.message.edit_reply_markup('')
-    id_ = int(call.data.split('_')[2])
-    archive(id_)
-    sp.update_table(ticket=get_ticket_by_id(id_), table='tickets')
-    await call.message.answer(f'Заявка отправлена в архив')
-
-@dp.message_handler(Text(equals='Мастера на удаление'), chat_type=types.ChatType.PRIVATE, state=Admin.default)
-async def get_masters_list_to_delete(message: types.Message, state: FSMContext):
-    masters = get_all_masters()
-
-    if not masters:
-        await message.answer("На данный момент нет мастеров")
-
-    else:
-        for data in masters:
-            text = f"<a href='tg://user?id={message.chat.id}'>" \
-                   f"{data['name']}</a>\n" \
-                   f"Контактный телефон: {data['phone']}" 
-
-            await message.answer(text, reply_markup=get_master_admin_cancel(data['uid'])) 
-
-@dp.callback_query_handler(Text(startswith=f'master_delete_'), chat_type=types.ChatType.PRIVATE, state='*')
-async def delete_chosen_master(call: types.CallbackQuery, state: FSMContext):
-    await call.message.edit_reply_markup('')
-    id_ = int(call.data.split('_')[2])
-    set_master_status_deleted(id_)
-    sp.update_table(master=get_master_by_uid(id_), table='masters')
-    await call.message.answer(f'Мастеру присвоена категория на удаление')
